bcfc83114a8e26de92f33b8fc582d430c5201fc8
Sabin <paudelsabin0@gmail.com>
2024-06-15 21:43:16 +0545

    Update revision.js

--- revision.js
+++ revision.js
@@ -6,3 +6,24 @@ for(let i=0;i<numbers.lengthl;i++){
   
 }
 console.log(range);
+
+
+
+//2nd 
+const arr=[1,2,3,4,5,6,7];
+
+const arr1=arr.map((arr)=>{
+  return arr*2;
+})
+
+const arr2=arr.filter((arr)=>{
+  return arr%2==0;
+})
+
+const arr3=arr.reduce((prev,curr)=>{
+  return prev+curr;
+})
+
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
